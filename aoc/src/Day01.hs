ab776c64cd07217248113e91ba48ab58bff68ee6
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2021-12-01 12:55:01 -0300

    small improvement

--- aoc/src/Day01.hs
+++ aoc/src/Day01.hs
@@ -2,10 +2,10 @@ module Day01
     ( day01
     ) where
 
-import Control.Comonad 
+import Control.Comonad ( Comonad(extend) ) 
 import qualified Data.List.NonEmpty as NE 
 import Data.List.NonEmpty (NonEmpty(..))
-import Data.Monoid 
+import Data.Monoid ( Sum(Sum, getSum) ) 
 
 slidingSums :: Int -> NonEmpty Int -> NonEmpty Int 
 slidingSums n = extend (sum . NE.take n) 
@@ -15,13 +15,12 @@ isIncreasing (x :| (y: _)) = y > x
 isIncreasing _             = False
 
 countIncreasing :: NonEmpty Int -> Int 
-countIncreasing = getSum . foldMap (Sum . b2i) . extend isIncreasing
+countIncreasing = getSum . foldMap (Sum . fromEnum) . extend isIncreasing
 
-b2i :: Bool -> Int 
-b2i True  = 1 
-b2i False = 0
-
+partA :: NonEmpty Int -> IO ()
 partA = print . countIncreasing
+
+partB :: NonEmpty Int -> IO ()
 partB = print . countIncreasing . slidingSums 3
 
 day01 :: IO ()
